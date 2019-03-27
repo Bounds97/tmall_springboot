edc10bd300b021024203bc7258cdbb8dbdb7c48e
Bounds97 <1455973223@qq.com>
2019-03-27 23:49:34 +0800

    加入分类信息的分页功能

--- tmall/src/main/java/top/bounds/tmall/service/CategoryService.java
+++ tmall/src/main/java/top/bounds/tmall/service/CategoryService.java
@@ -3,11 +3,15 @@ package top.bounds.tmall.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import top.bounds.tmall.dao.CategoryDAO;
 import top.bounds.tmall.pojo.Category;
+import top.bounds.tmall.util.Page4Navigator;
 
 /**
  * @author Bounds
@@ -18,6 +22,23 @@ import top.bounds.tmall.pojo.Category;
 public class CategoryService {
 	@Autowired
 	CategoryDAO categoryDAO;
+
+	/**
+	 * @param start
+	 * @param size
+	 * @param navigatePages
+	 * @return 为分类数据提供分页
+	 */
+	public Page4Navigator<Category> list(int start, int size, int navigatePages) {
+		//设置倒排序
+		Sort sort = new Sort(Sort.Direction.DESC, "id");
+		//根据start,size和sort创建分页对象
+		Pageable pageable = new PageRequest(start, size, sort);
+		//CategoryDAO根据这个分页对象获取结果page. 
+		Page<Category> pageFromJPA = categoryDAO.findAll(pageable);
+		return new Page4Navigator<>(pageFromJPA, navigatePages);
+
+	}
 
 	/**
 	 * @return 查找所有分类，并通过分类id倒排序
--- tmall/src/main/java/top/bounds/tmall/web/CategoryController.java
+++ tmall/src/main/java/top/bounds/tmall/web/CategoryController.java
@@ -1,13 +1,13 @@
 package top.bounds.tmall.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import top.bounds.tmall.pojo.Category;
 import top.bounds.tmall.service.CategoryService;
+import top.bounds.tmall.util.Page4Navigator;
 
 /**
  * @author Bounds
@@ -23,7 +23,13 @@ public class CategoryController {
 	 * @throws Exception
 	 */
 	@GetMapping("/categories")
-	public List<Category> list() throws Exception {
-		return categoryService.list();
+	public Page4Navigator<Category> list(@RequestParam(value = "start", defaultValue = "0") int start,
+			@RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+		// 如果 start 为负，那么修改为0. 这个事情会发生在当前是首页，并点击了上一页的时候
+		start = start < 0 ? 0 : start;
+		// 5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+		Page4Navigator<Category> page = categoryService.list(start, size, 5);
+		// 在这个page对象里，不仅包含了分页信息，还包含了数据信息，即有哪些分类数据。 这个可以通过getContent()获取出来。
+		return page;
 	}
 }
